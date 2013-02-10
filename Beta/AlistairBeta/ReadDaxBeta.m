635efc6acb3943f460938653766b4e8e8c42d0e2
Alistair Boettiger <boettiger@fas.harvard.edu>
2013-02-09 19:32:19 -0500

    New approach to ReadDax that uses memory map to enable specific pixel grabbing.

---
+++ Beta/AlistairBeta/ReadDaxBeta.m
@@ -0,0 +1,313 @@
+function [movie, infoFile] = ReadDaxBeta(varargin)
+% A faster and more flexible way to read in dax files.  
+% Only those frames which we'll read data from get mapped
+% Only the parts of those frames that we indicate get read into memory.  
+%--------------------------------------------------------------------------
+% [movie, infoFiles] = ReadDax(fileName, varargin)
+% This function loads a STORM movies from the dax file associated with the
+% provided .inf file
+%--------------------------------------------------------------------------
+% Outputs:
+% movies/LxMxN array: A 3D array containing the specified movie
+%--------------------------------------------------------------------------
+% Input:
+% fileName/string or structure: Either a path to the dax or inf file or an
+%   infoFile structure specifying the dax file to load
+%
+%--------------------------------------------------------------------------
+% Variable Inputs:
+%
+% 'file'/string ([]): A path to the associated .inf file
+%
+% 'path'/string ([]): Default path to look for .inf files
+%
+% 'startFrame'/double  (1): first of movie to load.  
+%
+% 'endFrame'/double ([]): last frame of the movie to load.  If empty will
+% be max. 
+%
+% 'subregion'/double (zeros(4,1)):  [xmin, xmax, ymin, ymax] of the region
+% of the dax file to load.  Pixels indexed from upper left, as in images.  
+%
+% 'infoFile'/info file structure ([]): An info file for
+% the files to be loaded.  
+%
+% 'verbose'/boolean (true): Display or hide function progress
+%
+% 'orientation'/string ('normal'): Control the relative rotation of the data
+%   structure
+%--------------------------------------------------------------------------
+% Alistair Boettiger & Jeffrey Moffitt
+% boettiger.alistair@gmail.com   jeffmoffitt@gmail.com
+% Copyright CC BY.   February 09, 2013.  
+%
+% Version 1.2
+%-------------------Updates:
+% Version 1.2
+% 02/09/13 modified to use memory map and allow arbitrary window as well as
+% an arbitrary start and end frame.  This is necessary to enable processing
+% a small region of a dax movie (e.g. by DaoSTORM/insight).  -- Alistair
+% Version 1.1
+% 01/19/13 modified to allow arbitrary start and end frame to be specified
+% by the user.  Removed 'image_dimension' flag (this was non-functional)
+% and removed allFrames (this has become redundant);  -- Alistair 
+% Version 1.0  
+% 09/07/12 original version -- Jeff 
+%--------------------------------------------------------------------------
+
+%--------------------------------------------------------------------------
+% Hardcoded Variables
+%--------------------------------------------------------------------------
+quiet = 0;
+orientationValues = {'normal'};
+flags = {'file', 'infoFile', 'startFrame','endFrame', 'verbose', ...
+    'orientation', 'path','allFrames'};
+
+%--------------------------------------------------------------------------
+% Global Variables
+%--------------------------------------------------------------------------
+global defaultDataPath;
+
+%--------------------------------------------------------------------------
+% Default Variables
+%--------------------------------------------------------------------------
+dataPath = defaultDataPath;
+allFrames = [];
+startFrame = 1;
+endFrame = []; 
+fileName = [];
+infoFile = [];
+verbose = true;
+orientation = 'normal';
+subregion = zeros(4,1); 
+
+%--------------------------------------------------------------------------
+% Parse Required Input
+%--------------------------------------------------------------------------
+if nargin >= 1
+    if isstruct(varargin{1})
+        infoFile = varargin{1};
+        varargin = varargin(2:end);
+    elseif ~ismember(varargin{1}, flags)
+        fileName =varargin{1};
+        varargin = varargin(2:end);
+    end
+end
+
+%--------------------------------------------------------------------------
+% Parse Variable Input
+%--------------------------------------------------------------------------
+if (mod(length(varargin), 2) ~= 0 ),
+    error(['Extra Parameters passed to the function ''' mfilename ''' must be passed in pairs.']);
+end
+parameterCount = length(varargin)/2;
+
+for parameterIndex = 1:parameterCount,
+    parameterName = varargin{parameterIndex*2 - 1};
+    parameterValue = varargin{parameterIndex*2};
+    switch parameterName
+        case 'file'
+            fileName = CheckParameter(parameterValue, 'string', 'file');
+        case 'allFrames'
+             allFrames = CheckParameter(parameterValue, 'boolean', 'allFrames');
+        case 'startFrame'
+            startFrame = CheckParameter(parameterValue, 'positive', 'startFrame');
+        case 'endFrame'
+            endFrame = CheckParameter(parameterValue, 'positive', 'endFrame');
+        case 'subregion'
+            subregion = CheckParameter(parameterValue,'array','subregion');
+        case 'infoFile'
+            infoFile = CheckParameter(parameterValue, 'struct', 'infoFile');
+        case 'verbose'
+            verbose = CheckParameter(parameterValue, 'boolean', 'verbose');
+        case 'orientation'
+            orientation = CheckList(parameterValue, orientationValues, 'orientation');
+        case 'path'
+            dataPath = CheckParameter(parameterValue, 'string', 'path');
+        otherwise
+            error(['The parameter ''' parameterName ''' is not recognized by the function ''' mfilename '''.']);
+    end
+end
+
+%--------------------------------------------------------------------------
+% Check parameter consistency
+%--------------------------------------------------------------------------
+if ~isempty(infoFile) && ~isempty(fileName)
+    error('You cannot specify info files and file names');
+end
+
+
+ 
+%--------------------------------------------------------------------------
+% Load info files if needed
+%--------------------------------------------------------------------------
+if isempty(infoFile)
+    if isempty(fileName)
+        infoFile = ReadInfoFile('path', dataPath, 'verbose', false);
+    else
+        infoFile = ReadInfoFile(fileName, 'verbose', false);
+    end
+    
+    if isempty(infoFile)
+        display('Canceled');
+        movie = {};
+        return;
+    end 
+end
+
+%--------------------------------------------------------------------------
+% Load Dax Files
+%--------------------------------------------------------------------------
+
+TFrames = infoFile.number_of_frames;
+frameSize = infoFile.frame_size;
+frameDim = infoFile.frame_dimensions;
+
+
+
+% Determine number of frames to load
+
+% parse now outdated option 'allFrames' for backwards compatability
+if ~isempty(allFrames) 
+    if allFrames
+        endFrame = TFrames;
+    else % first frame only 
+        endFrame = 1;
+        startFrame = 1; 
+    end
+end
+    
+if isempty(endFrame)
+    endFrame = TFrames;
+end
+if endFrame > TFrames;
+    warning(['input endFrame greater than total frames in dax_file.',...
+        'Using all available frames after ', num2str(startFrame)]);
+    endFrame = TFrames;  
+end
+numFrames = endFrame - startFrame + 1;
+
+fileName = [infoFile.localName(1:(end-4)) '.dax'];
+if verbose
+    display(['Loading ' infoFile.localPath fileName ]);
+end
+
+% parse subregion 
+%--------------------------------------------------------------------
+    xi = uint32(subregion(1));
+    xe = uint32(subregion(2));
+    yi = uint32(subregion(3));
+    ye = uint32(subregion(4));
+    
+    if xi == 0 
+        xi = uint32(1);
+    end
+    if xe == 0
+        xe = uint32(frameDim(1));
+    end
+    if yi == 0
+        yi = uint32(1);
+    end
+    if ye == 0
+        ye = uint32(frameDim(2));
+    end
+    
+    xs = xe-xi+uint32(1);
+    ys = ye-yi+uint32(1);
+    fi = uint32(1);
+    fe = uint32(numFrames);
+
+
+%------------------ arbitrary region ------------------------
+memoryMap = memmapfile([infoFile.localPath fileName], ...
+        'Format', 'int16', ...
+        'Writable', false, ...
+        'Offset', (startFrame-1)*frameSize, ...
+        'Repeat', numFrames*frameSize);
+    
+    % Let's hope these steps remain fast;  
+[ri,ci,zi] = meshgrid(xi:xe,yi:ye,fi:fe);
+ind = sub2ind([frameDim(1),frameDim(2),TFrames],ri(:),ci(:),zi(:));
+ind = sort(ind); 
+movie = memoryMap.Data(ind);  clear ind; %keeping memory small
+movie = swapbytes(movie);
+movie = reshape(movie,[xs,ys,numFrames]);
+if strcmp(orientation,'normal')
+ movie = permute(reshape(movie, [xs,ys,numFrames]), [2 1 3]);
+end
+  % figure(9); clf; imagesc(movie(:,:,1)); colorbar;
+
+%--------------------------------------------------
+
+
+
+%------------------ QuadViewRegion ------------------------
+memoryMap = memmapfile([infoFile.localPath fileName], ...
+        'Format', 'int16', ...
+        'Writable', false, ...
+        'Offset', (startFrame-1)*frameSize, ...
+        'Repeat', numFrames*frameSize);
+      
+upperleft = multiparse([1,frameDim(1)/2,1,frameDim(2)/2],memoryMap,orientation);
+upperright =  multiparse([frameDim(1)/2+1,frameDim(1),1,frameDim(2)/2],memoryMap,orientation);
+lowerleft = multiparse([1,frameDim(1)/2,frameDim(2)/2+1,frameDim(2)],memoryMap,orientation); % lowerleft
+lowerright = multiparse([frameDim(1)/2+1,frameDim(1),frameDim(2)/2+1,frameDim(2)],memoryMap,orientation); % lowerright
+movie = {upperleft,upperright,lowerleft,lowerright};
+%--------------------------------------------------
+
+if verbose
+    display(['Loaded ' infoFile.localPath fileName ]);
+    display([num2str(numFrames) ' ' num2str(frameDim(1)) ' x ' num2str(frameDim(2)) ...
+        ' frames loaded']);
+end
+
+
+function movie = multiparse(sr,memoryMap,orientation)
+[ri,ci,zi] = meshgrid(sr(1):sr(2),sr(3):sr(4),fi:fe);
+inds = sub2ind([frameDim(1),frameDim(2),TFrames],ri(:),ci(:),zi(:));
+movie = memoryMap.Data(sort(inds)); 
+movie = swapbytes(movie);
+movie = reshape(movie,[xs,ys,numFrames]);
+if strcmp(orientation,'normal')
+ movie = permute(reshape(movie, [xs,ys,numFrames]), [2 1 3]);
+end
+
+
+%  % Old memory maps (to delete in later update) 
+% 
+% %------------------ top bottom split ------------------------
+% format = {...
+%     'int16' [frameDim(1),frameDim(2)/2] 'top'; ...
+%     'int16' [frameDim(1),frameDim(2)/2] 'bottom';};
+% Offset = startFrame-1;
+% 
+% memoryMap = memmapfile([infoFile.localPath fileName], ...
+%         'Format', format, ...
+%         'Writable', false, ...
+%         'Offset', Offset, ...
+%         'Repeat', numFrames);
+% 
+% toponly = swapbytes(memoryMap.Data(3).top');
+% 
+% %--------------------------------------------------
+% 
+% % --------------- left right split ------------------
+% format = {...
+%     'uint16' [frameDim(1)/2,1] 'values';};
+% Offset = (startFrame-1)*frameDim(2);
+% 
+% memoryMap = memmapfile([infoFile.localPath fileName], ...
+%         'Format', format, ...
+%         'Writable', false, ...
+%         'Offset', Offset, ...
+%         'Repeat', numFrames*frameDim(2)*2);
+% 
+% Dax = memoryMap.Data(2:2:end);
+% movie = cat(1,Dax.values);
+% movie = swapbytes(reshape(movie,[frameDim(1)/2,frameDim(2),numFrames]));
+% movie = permute(reshape(movie, [frameDim(1)/2,frameDim(2),numFrames]), [2 1 3]);
+%  figure(1); clf; imagesc(movie(:,:,2))
+% %------------------------------------------------------------------------
+% 
+% 
+
