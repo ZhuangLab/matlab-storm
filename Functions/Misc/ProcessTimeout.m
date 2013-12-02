65047baf1e69487b7e7d326e12e832bc4a04ec1b
Alistair Boettiger <boettiger@fas.harvard.edu>
2013-12-02 11:38:11 -0500

    Debug ProcessTimeout to avoid errors when no processes are found.

--- Functions/Misc/ProcessTimeout.m
+++ Functions/Misc/ProcessTimeout.m
@@ -37,29 +37,37 @@ end
 % date-string is formatted: YYYYMMDDhhmmss
 
 [~,A] = system(['wmic process where name=','"',processName,'"']);
-C = strsplit(A,'\n');
 
-dateStart = strfind(C{1},'CreationDate');
-dateEnd = strfind(C{1},'CSCreationClassName')-2;
-idStart = strfind(C{1},' ProcessId ');
-idEnd = strfind(C{1},' QuotaNonPagedPoolUsage')-2;
-c = clock; 
-currTime = c(4)*60*60+c(5)*60 + c(6);
-for i=1:length(C)-3
-    dateString = C{i+1}(dateStart:dateEnd);
-    idString = C{i+1}(idStart:idEnd);
-%     yearS = dateString(1:4); 
-%     monthS = dateString(5:6);
-%     dayS = dateString(7:8);
-    hourS = dateString(9:10);
-    minS  = dateString(11:12);
-    secS = dateString(13:14);
-    startTime = str2double(hourS)*60*60+str2double(minS)*60+str2double(secS);
-    if currTime - startTime > maxTime;
-        stopProcess = ['wmic process ',idString,' delete '];
-        if verbose
-            disp(['aborting process ',idString]); 
+if isempty(strfind(A,'No Instance(s) Available'))
+    C = strsplit(A,'\n');
+
+    dateStart = strfind(C{1},'CreationDate');
+    dateEnd = strfind(C{1},'CSCreationClassName')-2;
+    idStart = strfind(C{1},' ProcessId ');
+    idEnd = strfind(C{1},' QuotaNonPagedPoolUsage')-2;
+    c = clock; 
+    currTime = c(4)*60*60+c(5)*60 + c(6);
+    for i=1:length(C)-3
+        dateString = C{i+1}(dateStart:dateEnd);
+        idString = C{i+1}(idStart:idEnd);
+    %     yearS = dateString(1:4); 
+    %     monthS = dateString(5:6);
+    %     dayS = dateString(7:8);
+        hourS = dateString(9:10);
+        minS  = dateString(11:12);
+        secS = dateString(13:14);
+        startTime = str2double(hourS)*60*60+str2double(minS)*60+str2double(secS);
+        if currTime - startTime > maxTime;
+            stopProcess = ['wmic process ',idString,' delete '];
+            if verbose
+                disp(['aborting process ',idString]); 
+            end
+            dos(stopProcess);
         end
-        dos(stopProcess);
     end
+else
+if verbose
+   disp([processName, ' is not currently running on this Windows machine.']); 
 end
+end
+
