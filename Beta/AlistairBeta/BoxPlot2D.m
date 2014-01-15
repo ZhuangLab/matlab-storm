049417ba5dd3f7ca09d39223f5db39f43e84df34
Alistair Boettiger <boettiger@fas.harvard.edu>
2014-01-15 16:48:51 -0500

    new plotting functions for BoxPlots

---
+++ Beta/AlistairBeta/BoxPlot2D.m
@@ -0,0 +1,83 @@
+function BoxPlot2D(x,data,varargin)
+% BoxPlot2D(x,d) 
+% generates a box plot for the data in each cell entry of d{i}, plotted at
+% position x(i).
+%
+%--------------------------------------------------------------------------
+% Optional Inputs
+% 'flag' /
+% 'datanames' / cell / {}
+% 'clrmap' / string or colormap
+
+%% Main Function
+
+%% Default parameters
+width = .03;
+clrmap = [];
+datanames = {}; 
+
+
+
+%--------------------------------------------------------------------------
+%% Parse Variable Input Parameters
+%--------------------------------------------------------------------------
+if nargin > 1
+    if (mod(length(varargin), 2) ~= 0 ),
+        error(['Extra Parameters passed to the function ''' mfilename ''' must be passed in pairs.']);
+    end
+    parameterCount = length(varargin)/2;
+
+    for parameterIndex = 1:parameterCount,
+        parameterName = varargin{parameterIndex*2 - 1};
+        parameterValue = varargin{parameterIndex*2};
+        switch parameterName
+            case 'width'
+                width = CheckParameter(parameterValue,'fraction','width');
+            case 'datanames'
+                datanames = CheckParameter(parameterValue, 'cell', 'datanames');
+            case 'colormap'
+                clrmap = CheckParameter(parameterValue,'colormap','colormap') ; % string name of colormap or a colormap matrix.
+            otherwise
+                error(['The parameter ''', parameterName,...
+                    ''' is not recognized by the function, ''',...
+                    mfilename '''.' '  See help ' mfilename]);
+        end
+    end
+end
+
+
+%--------------------------------------------------------------------------
+%% Actual Function
+%--------------------------------------------------------------------------
+
+xRange = max(x) - min(x);
+numDataTypes = length(data);
+w = xRange*width;
+
+if isempty(clrmap)
+    clrmap = repmat([0,0,1],numDataTypes,1);
+end
+if ischar(clrmap)
+    try
+    clrmap = eval([clrmap,'(numDataTypes)']);
+    catch 
+       disp([clrmap,' is not a valid colormap name']);  
+    end
+end
+
+quartiles = zeros(numDataTypes,2);
+medians = zeros(numDataTypes,1); 
+for i=1:numDataTypes
+    quartiles(i,:) = quantile(data{i},[.25,.75]);
+    quarts = quantile(data{i},[.25,.75]);
+    medians(i) = median(data{i});
+    boxes = [x(i)-w/2,quarts(1),w,quarts(2)-quarts(1)];  
+    
+    hold on;
+    rectangle('Position',boxes,'EdgeColor',clrmap(i,:)); 
+    plot(x(i),medians(i),'.','color',clrmap(i,:),'MarkerSize',20);
+    if ~isempty(datanames)
+        text(x(i)+w,medians(i),datanames{i});
+    end
+end
+
