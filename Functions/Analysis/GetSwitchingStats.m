568a26c6e38db911bc3e02a607d1dedccfab096f
Alistair Boettiger <boettiger@fas.harvard.edu>
2014-03-24 13:22:57 -0400

    updated GetSwitchingStats with some new analysis options

--- Functions/Analysis/GetSwitchingStats.m
+++ Functions/Analysis/GetSwitchingStats.m
@@ -1,4 +1,4 @@
-function medStats = GetSwitchingStats(mlist,varargin)
+function [medStats,numCycles,filt] = GetSwitchingStats(mlist,varargin)
 %--------------------------------------------------------------------------
 % Returns statistics on molecule behavior in movie
 %     Distribution of # of cycles per dye / cluster
@@ -30,7 +30,7 @@ function medStats = GetSwitchingStats(mlist,varargin)
 %--------------------------------------------------------------------------
 %% Default Variables
 %--------------------------------------------------------------------------
-boxSize = 10; % nm % to determine cluster centroids.  
+boxSize = 20; % nm % to determine cluster centroids.  
 minLoc = 2; % min localizations to be called a switching cluster
 clusterWidth = 1; % pixels (distance over which to cluster localizations)
 startframe = 1; 
@@ -41,11 +41,12 @@ allSpots = false;
 plotsOn = true;
 roi = [];
 saveName = '';
+maxDots = 2500;
 
 %--------------------------------------------------------------------------
 %% Parse variable input
 %--------------------------------------------------------------------------
-if nargin > 3
+if nargin > 1
     if (mod(length(varargin), 2) ~= 0 ),
         error(['Extra Parameters passed to the function ''' mfilename ''' must be passed in pairs.']);
     end
@@ -76,12 +77,13 @@ if nargin > 3
                 roi = CheckParameter(parameterValue,'array','roi');
             case 'saveName'
                 saveName = CheckParameter(parameterValue,'string','saveName');
+            case 'maxDots'
+                maxDots  = CheckParameter(parameterValue,'string','maxDots');
             otherwise
                 error(['The parameter ''' parameterName ''' is not recognized by the function ''' mfilename '''.']);
         end
     end
 end
-
 
 if isempty(roi)
     roi = [0,256,0,256];
@@ -94,16 +96,23 @@ xBins = roi(1):(boxSize/npp):roi(2);
 mapIdx = hist3([mlist.y,mlist.x],{yBins,xBins});  
 % figure(2); clf; imagesc(mapIdx); caxis([0,3]);
 
-mapBW = mapIdx>=minLoc;  
-mapBW = imfill(mapBW,'holes'); 
+mapBW = mapIdx>=minLoc;  % figure(2); clf; imagesc(mapBW);
 props = regionprops(mapBW,mapIdx,'WeightedCentroid');
 cents = cat(1,props.WeightedCentroid)*boxSize/npp;
 
-numDots = length(props); 
+numDots = min(length(props),maxDots); 
 
 % Feducials must be ID'd in at least fmin fraction of total frames
 fb =[cents(:,1)-clusterWidth/2, cents(:,1)+clusterWidth/2,cents(:,2)-clusterWidth/2,cents(:,2)+clusterWidth/2];
 
+%  figure(2); clf; imagesc(mapIdx); caxis([0,3]);
+%  xhist = (mlist.xc-roi(1))*npp/boxSize+1;
+%  yhist = (mlist.yc-roi(3))*npp/boxSize+1;
+%  hold on; plot(xhist,yhist,'k.','MarkerSize',10);
+%  plot(cents(:,1)*npp/boxSize,cents(:,2)*npp/boxSize,'c+','MarkerSize',10);
+
+ % figure(3); clf; plot(mlist.xc(mlist.a*.43>2500),mlist.yc(mlist.a*.43>2500),'k.');
+ 
 dotFrames = cell(numDots,1); 
 keptSpots = false(length(mlist.x),1);
 for i=1:numDots
@@ -112,9 +121,11 @@ for i=1:numDots
         mlist.frame > startframe;
    dotFrames{i} = mlist.frame(inbox);
    keptSpots = keptSpots | inbox; 
+   if verbose && (rem(i,1000) == 0)
+       disp(i/numDots*100);
+   end
 end
 
-numCycles = cellfun(@length,dotFrames);
 
 %% 
 
@@ -123,6 +134,9 @@ if allSpots
 else
     filt = keptSpots;
 end
+
+numCycles = cellfun(@length,dotFrames);
+
 
 meanCycles = mean(numCycles); 
 medCycles = median(numCycles); 
@@ -143,18 +157,23 @@ lengthX = 0:100;
 
 % Plotting
 if plotsOn
-    subplot(2,2,1); hist(numCycles,cyclesX); 
+    subplot(3,2,1); hist(numCycles,cyclesX); 
     title(['Ave # Cycles=',num2str(meanCycles,3)]); xlim([0,max(cyclesX)]);
-    subplot(2,2,2); hist(double(mlist.a)*photonsPerCount,photonX); 
+    subplot(3,2,2); hist(double(mlist.a)*photonsPerCount,photonX); 
     title(['Ave # Photons=',num2str(meanPhotons,3)]); xlim([0,max(photonX)]);
-    subplot(2,2,3); hist(double(mlist.h),heightX); 
+    subplot(3,2,3); hist(double(mlist.h),heightX); 
     title(['Ave Height=',num2str(meanHeight,3)]); xlim([0,max(heightX)]);
-    subplot(2,2,4); hist(double(mlist.length),lengthX);
+    subplot(3,2,4); hist(double(mlist.length),lengthX);
     title(['Ave frames on=',num2str(meanFramesOn,3)]); xlim([0,max(lengthX)]);
+    subplot(3,2,5);  imagesc(mapIdx(50:150,50:150)); caxis([0,5]); axis off;
+    subplot(3,2,6); imagesc(0*mapIdx(50:150,50:150)); hold on;  colormap gray;
+                    plot((mlist.xc-roi(1))+1,(mlist.yc-roi(3))+1,'w.'); hold on;
+                    plot(cents(:,1),cents(:,2),'c+');
     PresentationPlot();
     figHand = gcf;
     if ~isempty(saveName)
-        saveas(figHand,savename); 
+        disp(['saving ',saveName]);
+        saveas(figHand,saveName); 
     end
 end
 
