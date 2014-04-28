4e2b908bf7887a89cd7f940cdc051f4792ada726
Alistair Boettiger <boettiger@fas.harvard.edu>
2014-04-28 10:11:09 -0400

    Removed Beta from matlab-storm

--- Beta/GUIs/ChromatinCropper/Menu/SpecifyWarp.m
+++
@@ -1,9 +0,0 @@
-function SpecifyWarp(handles)
-
-global CC
-
-[~,newFolder,notCancel] = uigetfile(CC{handles.gui_number}.pars1.BeadFolder,...
-    'Select chromewarps.mat file');
-if notCancel ~= 0 
-CC{handles.gui_number}.pars1.BeadFolder = newFolder; 
-end
