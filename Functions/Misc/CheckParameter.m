dc8e99bbf2c7004fc0bbf9be6b19fc3e8f08fc5f
Alistair Boettiger <boettiger@fas.harvard.edu>
2014-09-24 18:00:27 -0400

    Added new fields float and int to CheckParameter

--- Functions/Misc/CheckParameter.m
+++ Functions/Misc/CheckParameter.m
@@ -10,10 +10,10 @@ function value = CheckParameter(value, type, name)
 % type/string or cell array of strings: A flag to specify the check
 %   conditions or the set of conditions to test
 %   Valid types: 
-%           'positive',
-%           'nonnegative',
-%           'struct',
-%           'cell',
+%           'positive'
+%           'nonnegative'
+%           'struct'
+%           'cell'
 %           'string'
 %           'boolean'
 %           'array' 
@@ -24,6 +24,8 @@ function value = CheckParameter(value, type, name)
 %           'handle'
 %           'function'
 %           'map'
+%           'float'
+%           'integer'
 %           'freeType'
 % 
 % name/string: The name of the parameter to be checked
@@ -129,6 +131,10 @@ for i=1:length(type)
         case 'map'
             if ~strcmp(class(value), 'containers.Map')
                 error([name ' is not a containers.Map object']);
+            end
+        case 'float'
+            if ~isfloat(value)
+                error([name 'is not a float']); 
             end
         case 'freeType'
         otherwise
